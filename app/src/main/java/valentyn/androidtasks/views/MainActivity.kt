984ba229408c734706ea13ecc8250e4fb9d2010a
valentinchor <zeratul89>
2019-02-01 16:22:20 +0200

    Splash screen fix

--- app/src/main/java/valentyn/androidtasks/views/MainActivity.kt
+++ app/src/main/java/valentyn/androidtasks/views/MainActivity.kt
@@ -4,21 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import io.realm.Realm
-import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_main.*
 import valentyn.androidtasks.R
 import valentyn.androidtasks.adapters.FragmentsPagerAdapter
-import valentyn.androidtasks.repository.RealmRepository
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Realm.init(this)
-        Realm.setDefaultConfiguration(RealmConfiguration.Builder().build())
-        RealmRepository.initDB()
 
         setSupportActionBar(toolbar)
 
--- app/src/main/java/valentyn/androidtasks/views/SplashScreenActivity.kt
+++ app/src/main/java/valentyn/androidtasks/views/SplashScreenActivity.kt
@@ -1,40 +1,47 @@
 package valentyn.androidtasks.views
 
-import android.support.v7.app.AppCompatActivity
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import valentyn.androidtasks.R
-import android.widget.ProgressBar
 import android.os.Bundle
+import android.os.Handler
+import io.realm.Realm
+import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_splash_screen.*
-import java.util.*
+import valentyn.androidtasks.repository.RealmRepository
 
 class SplashScreenActivity : AppCompatActivity() {
-    private var timer: Timer? = null
-    private var progressBar: ProgressBar? = null
-    private var i = 0
+    private var progressStatus = 0
+    private var handler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        progressBar?.progress = 0
+        Realm.init(this)
+        Realm.setDefaultConfiguration(RealmConfiguration.Builder().build())
+        RealmRepository.initDB()
 
-        val period: Long = 100
-        timer = Timer()
-        timer?.schedule(object : TimerTask() {
-            override fun run() {
-                if (i < 100) {
-                    runOnUiThread { textView.text = "$i%" }
-                    progressBar?.progress = i
-                    i++
-                } else {
-                    timer?.cancel()
-                    val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                }
-            }
-        }, 0, period)
+        handler = Handler(Handler.Callback {
+            progressStatus += 10
+            progressBarHorizontal.progress = progressStatus
+            textViewProgressBar.text = "${progressStatus}/${progressBarHorizontal.max}"
+
+            if (progressStatus == 100) {
+                startActivity(
+                    Intent(
+                        this@SplashScreenActivity,
+                        MainActivity::class.java
+                    )
+                )
+                finish()
+
+            } else handler?.sendEmptyMessageDelayed(0, 100)
+
+            true
+        })
+
+        handler?.sendEmptyMessage(0)
     }
 
 }
