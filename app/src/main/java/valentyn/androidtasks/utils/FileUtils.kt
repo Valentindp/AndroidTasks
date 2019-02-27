f91050f9da7c95fc738b1e8b18343b114ba70e3a
Valentyn Chornyi <valentindp.post@gmail.com>
2019-02-27 14:06:15 +0200

    FileProvider

--- app/src/main/java/valentyn/androidtasks/utils/FileUtils.kt
+++ app/src/main/java/valentyn/androidtasks/utils/FileUtils.kt
@@ -1,39 +1,18 @@
 package valentyn.androidtasks.utils
 
 import android.content.Context
-import android.content.Intent
+
 import android.net.Uri
 import android.os.Environment
-import android.provider.MediaStore
-import android.support.v4.content.FileProvider
-import android.widget.Toast
 import java.io.File
-import java.io.IOException
 import java.text.DateFormat
+import android.support.v4.content.FileProvider.getUriForFile
+
 
 object FileUtils {
 
-    private fun createImageFile(context: Context): File = File.createTempFile(
-        "JPEG_" + DateFormat.getDateTimeInstance() + "_",
-        ".jpg",
-        context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-    )
-
     fun getPhotoURI(context: Context): Uri? {
-        var photoFile: File? = null
-        var photoURI: Uri? = null
-        try {
-            photoFile = FileUtils.createImageFile(context)
-        } catch (ex: IOException) {
-            Toast.makeText(context, "Error!", Toast.LENGTH_SHORT).show()
-        }
-        if (photoFile != null) {
-            photoURI = FileProvider.getUriForFile(
-                context,
-                "valentyn.androidtasks.android.provider",
-                photoFile
-            )
-        }
-        return photoURI
+        val newFile = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "image" + DateFormat.getDateTimeInstance() + ".jpg")
+        return getUriForFile(context, "valentyn.androidtasks.fileprovider", newFile)
     }
 }
